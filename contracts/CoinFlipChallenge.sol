0eb038b03df2c996c6d5aad9eef988ea560bceff
0xkowloon <0xkowloon@protonmail.com>
2022-04-22 16:54:08 +0100

    style: update indentation

--- contracts/CoinFlipChallenge.sol
+++ contracts/CoinFlipChallenge.sol
@@ -1,11 +1,12 @@
 pragma solidity 0.8.13;
 
 interface ICoinFlip {
-  function flip(bool _guess) external returns (bool);
+    function flip(bool _guess) external returns (bool);
 }
 
 contract CoinFlipChallenge {
-    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    uint256 FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
     ICoinFlip coinFlipChallenge;
 
     constructor(address _coinFlip) {
@@ -13,9 +14,9 @@ contract CoinFlipChallenge {
     }
 
     function guess() external returns (bool) {
-      uint256 blockValue = uint256(blockhash(block.number - 1));
-      uint256 coinFlip = blockValue / FACTOR;
-      bool side = coinFlip == 1 ? true : false;
-      return coinFlipChallenge.flip(side);
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        return coinFlipChallenge.flip(side);
     }
 }
--- contracts/TelephoneChallenge.sol
+++ contracts/TelephoneChallenge.sol
@@ -1,17 +1,17 @@
 pragma solidity 0.8.13;
 
 interface ITelephone {
-  function changeOwner(address _owner) external;
+    function changeOwner(address _owner) external;
 }
 
 contract TelephoneChallenge {
-  ITelephone public telephone;
+    ITelephone public telephone;
 
-  constructor(address _telephone) {
-    telephone = ITelephone(_telephone);
-  }
+    constructor(address _telephone) {
+        telephone = ITelephone(_telephone);
+    }
 
-  function changeOwner() external {
-    telephone.changeOwner(msg.sender);
-  }
+    function changeOwner() external {
+        telephone.changeOwner(msg.sender);
+    }
 }
